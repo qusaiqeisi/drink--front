89d1d4b16363413892cd1990405fc0dd3c078081
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-07-26 16:31:55 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './componant/Header';
 import Main from './componant/Main';
 import ShowFavDrink from './componant/ShowFavDrink'
 // import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Navbar,Nav } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   BrowserRouter as Router,
@@ -16,33 +17,34 @@ export class App extends Component {
   render() {
     return (
       <>
-      <Header />
-      <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/drinkFav">drinkFav</Link>
-            </li>
-          </ul>
-        </nav>
+        <Router>
+          <div>
+          <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">DrinkApp</Navbar.Brand>
+          <Nav className="me-auto">
+                <Nav.Link href="/">
+                <Link to="/">Home</Link>
+                </Nav.Link>
+               
+                <Nav.Link href="/drinkFav">
+                <Link to="/drinkFav">drinkFav</Link>
+                </Nav.Link>
+                </Nav>
+              </Navbar>
 
-        {/* A <Switch> looks through its children <Route>s and
+            {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/drinkFav">
-            <ShowFavDrink />
-          </Route>
-          <Route path="/">
-            <Main />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    </>
+            <Switch>
+              <Route path="/drinkFav">
+                <ShowFavDrink />
+              </Route>
+              <Route path="/">
+                <Main />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </>
     )
   }
 }
--- src/componant/Header.js
+++ src/componant/Header.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import { Navbar,Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-export class Header extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar bg="light" expand="lg">
+// import React, { Component } from 'react'
+// import { Navbar,Nav } from 'react-bootstrap'
+// import { Link } from 'react-router-dom'
+// export class Header extends Component {
+//     render() {
+//         return (
+//             // <div>
+//             //     <Navbar bg="light" expand="lg">
 
-                    <Navbar.Brand href="#home">DrinkApp</Navbar.Brand>
+//             //         <Navbar.Brand href="#home">DrinkApp</Navbar.Brand>
 
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/drinkFav">FaverDrink</Nav.Link>
-                    </Nav>
-                </Navbar>
-            </div>
+//             //         <Nav className="me-auto">
+//             //             <Nav.Link href="/">Home</Nav.Link>
+//             //             <Nav.Link href="/drinkFav">FaverDrink</Nav.Link>
+//             //         </Nav>
+//             //     </Navbar>
+//             // </div>
             
-        )
-    }
-}
+//         )
+//     }
+// }
 
-export default Header
+// export default Header
